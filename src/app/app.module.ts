f66eb6f58c569508ea6432727cdd160b041ccc67
aydnOktay <xaydinoktay@gmail.com>
2023-10-18 15:17:11 +0300

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/api/auth/auth.module';
+import { SessionsModule } from 'src/api/sessions/sessions.module';
 import { UserModule } from 'src/api/user/user.module';
 import { CredsModule } from 'src/services/creds/creds.module';
 
@@ -14,7 +15,7 @@ import { CredsModule } from 'src/services/creds/creds.module';
       password:"8105693a",
       database:"postgres",
       synchronize:true
-    }),AuthModule,UserModule,CredsModule
+    }),AuthModule,UserModule,CredsModule,SessionsModule
   ],
   controllers: [],
   providers: [],
